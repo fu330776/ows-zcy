d3dbf0a4f8d9e3b7bf27b4d60fa1959584ca97b8
17783374871@163.com <zhangxinr520lmy>
2019-05-22 20:10:42 +0800

    code 2019-5-22 20：00

--- src/main/java/com/goodsogood/ows/model/vo/AdminForm.java
+++ src/main/java/com/goodsogood/ows/model/vo/AdminForm.java
@@ -1,5 +1,7 @@
 package com.goodsogood.ows.model.vo;
-
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 @Data
 @ApiModel
 public class AdminForm {
